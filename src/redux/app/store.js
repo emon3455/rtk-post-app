9eb2afddde5bd346beb4582aaf46d240e850bca2
Emon <emon.mhk69@gmail.com>
2023-08-20 14:29:21 +0600

    store and slice creation done also provind is doen for the post

---
+++ src/redux/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { postApi } from '../services/postSlice'
+
+export const store = configureStore({
+    reducer: {
+        
+        [postApi.reducerPath]: postApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(postApi.middleware),
+})
+
+setupListeners(store.dispatch)
---
+++ src/redux/services/postSlice.js
@@ -0,0 +1,20 @@
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const postApi = createApi({
+    reducerPath: 'postApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+    endpoints: (builder) => ({
+
+      getAllPosts: builder.query({
+        query: () => ({
+            url: "/posts",
+            method: "GET"
+        }),
+      }),
+
+    }),
+})
+
+export const {useGetAllPostsQuery} = postApi;
+
